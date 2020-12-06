8a1fbd1b7a81ac76cf4db4fc493019fec2c11d08
Aaron <aaronlhma@gmail.com>
2020-12-05 17:05:02 -0800

    adds js function to switch dark/light

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,10 +1,29 @@
 function dark_theme() {
     let x = document.getElementsByClassName("Light");
-    x.className ="Dark";
+    for (i=0;i<x.length-1;i++){
+        x[i].className ="Dark"
+    }
+    console.log(i)
 }
 function light_theme(){
     let x = document.getElementsByClassName("Dark");
-    x.className="Light";
+    for(i=0;i<x.length-1;i++){
+        x[i].className="Light"
+    }
+    console.log(i)
+}
+function switch_theme(){
+    x = document.getElementById("Theme");
+    if (x.className=="Dark"){
+        light_theme();
+        console.log('Dark')
+    }
+    else{
+        dark_theme();
+        console.log('Light');
+    }
 }
 
-document.getElementById("Theme").addEventListener("click",dark_theme());
+let x = document.getElementById("Theme");
+x.addEventListener("click",switch_theme);
+console.log(x.className);
