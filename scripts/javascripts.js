980f373dadaabd10d1234199b87463615594bd9a
Aaron <aaronlhma@gmail.com>
2020-12-05 19:51:09 -0800

    adds show/hide functionality to newnote and cancel buttons

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,29 +1,57 @@
 function dark_theme() {
     let x = document.getElementsByClassName("Light");
-    for (i=0;i<x.length-1;i++){
-        x[i].className ="Dark"
+    while (x.length > 0){
+        x[0].classList.add('Dark');
+        x[0].classList.remove('Light');
     }
-    console.log(i)
+    document.getElementById("Theme").innerText='Light Theme';
 }
+
 function light_theme(){
     let x = document.getElementsByClassName("Dark");
-    for(i=0;i<x.length-1;i++){
-        x[i].className="Light"
+    while(x.length > 0){
+        x[0].classList.add('Light');
+        x[0].classList.remove('Dark');
     }
-    console.log(i)
+    document.getElementById("Theme").innerText='Dark Theme'
 }
+
 function switch_theme(){
     x = document.getElementById("Theme");
     if (x.className=="Dark"){
         light_theme();
-        console.log('Dark')
     }
     else{
         dark_theme();
-        console.log('Light');
     }
 }
 
-let x = document.getElementById("Theme");
-x.addEventListener("click",switch_theme);
-console.log(x.className);
+function hide_buttons(){
+    let x = document.getElementsByClassName("Show");
+    while(x.length>0){
+        x[0].style.visibility='hidden';
+        x[0].classList.add('Hide')
+        x[0].classList.remove('Show')
+    }
+}
+
+function show_buttons(){
+    let x = document.getElementsByClassName("Hide");
+    while(x.length>0){
+        x[0].style.visibility='visible';
+        x[0].classList.add('Show')
+        x[0].classList.remove('Hide')
+    }
+
+}
+
+function main(){
+    let ThemeToggle = document.getElementById("Theme");
+    ThemeToggle.addEventListener("click",switch_theme);
+    let Cancel = document.getElementById("Cancel");
+    Cancel.addEventListener("click",hide_buttons)
+    let NewNote = document.getElementById("NewNote");
+    NewNote.addEventListener("click",show_buttons)
+}
+
+main()
